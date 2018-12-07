87266126d2fb2a4177307de462d2369fb477a028
Eddy Jo <edwardjo811@gmail.com>
2018-12-07 09:02:17 -0500

    fetch data using HTTP

--- services-and-http/src/app/app.module.ts
+++ services-and-http/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 import { EmployeeService } from './employee.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { EmployeeService } from './employee.service';
     EmployeeDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
